66723541eb50ef21a57c12f0ea2ddd854c193180
goodwin5300 <goodwin5300@uwlax.edu>
2021-09-10 13:36:51 -0500

    Created projected

---
+++ app/src/main/java/com/example/collectingsensordata/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.collectingsensordata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    //used to determine if we are currently collecting data or not
+    private val collecting: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //objects from xml
+
+    }
+}
